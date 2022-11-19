848d05d641906590f2ca9e955a820462c508f9af
jose antonio <jsancesc@gmail.com>
2022-11-19 19:30:06 +0100

    juegoAMedias

--- Blackjack.py
+++ Blackjack.py
@@ -32,9 +32,54 @@ def pedir_carta():
     valor2 = list(cartas.values())[eleccion2]
     print(carta1, valor1)
     print("Sus dos cartas son",carta1, carta2)
+    print(cartas)
     return "Cuyo valor es:", (valor1 + valor2)
-
-print(pedir_carta())
+def carta_crupier():
+    carta3 = listaDeCartas[2]
+    valor3 = cartas[carta3]
+    carta4 = listaDeCartas[3]
+    valor4 = cartas[carta4]
+    return carta3, carta4,"cuyo valor es:", (valor3 + valor4)
+def pedir_carta2():
+    carta1 = listaDeCartas[0]
+    valor1 = cartas[carta1]
+    carta2 = listaDeCartas[1]
+    valor2 = cartas[carta2]
+    return carta1, carta2,"cuyo valor es:", (valor1 + valor2)
+def pedir_tercera_carta():
+    carta1 = listaDeCartas[0]
+    valor1 = cartas[carta1]
+    carta2 = listaDeCartas[1]
+    valor2 = cartas[carta2]
+    carta3 = listaDeCartas[4]
+    valor3 = cartas[carta3]
+    return carta1, carta2, carta3,"cuyo valor es:", (valor1 + valor2 + valor3)
+def juego():
+    print("Bienvenido al juego de Blackjack")
+    print("¿Quieres jugar?")
+    print("1. Si")
+    print("2. No")
+    eleccion = input("Elige una opción: ")
+    try:
+        eleccion = int(eleccion)
+    except ValueError:
+        print("No has introducido un número")
+        juego()
+    if eleccion == 1:
+        shuffle(listaDeCartas)
+        print("Las dos cartas del crupier son",carta_crupier())
+        print("Sus dos cartas son",pedir_carta2())
+        print("¿Quieres otra carta?")
+        print("1. Si")
+        print("2. No")
+        eleccion2 = int(input("Elige una opción: "))
+        if eleccion2 == 1:
+            print("Sus dos cartas son",pedir_tercera_carta())
+        else:
+            pass
+    else:
+        pass
+print(juego())
 """print(coger_cartas())"""
 """choice(cartas)
 sample(cartas, 5) """
